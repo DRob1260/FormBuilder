5bb6f209d5e79f6ef8f91b343031668c2542f89f
Robert, Drew <ddrobe2@ilstu.edu>
2020-02-09 18:04:07 -0600

    reformatted code

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import { FormBuilderHeader } from "./components/FormBuilderHeader/FormBuilderHeader";
-import { FormBuilderBody } from './components/FormBuilderBody/FormBuilderBody';
-import { FormBuilderFooter } from './components/FormBuilderFooter/FormBuilderFooter';
+import {FormBuilderHeader} from './components/FormBuilderHeader/FormBuilderHeader';
+import {FormBuilderBody} from './components/FormBuilderBody/FormBuilderBody';
+import {FormBuilderFooter} from './components/FormBuilderFooter/FormBuilderFooter';
 import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return (
-      <>
-      <div className="App" style={{minHeight: '100vh'}}>
-        <div id={'formBuilderHeader'}>
-            <FormBuilderHeader/>
-        </div>
-        <div id={'formBuilderBody'}>
-            <FormBuilderBody/>
-        </div>
-        <div id={'formBuilderFooter'}>
-            <FormBuilderFooter/>
-        </div>
-      </div>
-      </>
-  );
+    return (
+        <>
+            <div className="App" style={{minHeight: '100vh'}}>
+                <div id={'formBuilderHeader'}>
+                    <FormBuilderHeader/>
+                </div>
+                <div id={'formBuilderBody'}>
+                    <FormBuilderBody/>
+                </div>
+                <div id={'formBuilderFooter'}>
+                    <FormBuilderFooter/>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export {App};
